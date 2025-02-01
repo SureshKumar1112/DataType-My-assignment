673f87061d76a889cc43bc41be0ec753b53da4e5
SureshKumar1112 <sureshskc2016@gmail.com>
2025-02-01 17:17:30 +0530

    Add files via upload
    day1

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float value=91.0f;
+				System.out.println(value);
+				float marks=93.45679f;
+				System.out.println(marks);
+				String data = "Google";
+				System.out.println(data);
+				boolean chromeDoesNotSupportJava=false;
+				System.out.println(chromeDoesNotSupportJava);
+				int year=2008;
+				System.out.println(year);
+				char shortCutKey='c';
+				System.out.println(shortCutKey);
+
+	}
+
+}
